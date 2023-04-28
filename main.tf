5c4cc932bb500b2fe85f930bcd97046a81594501
Andrei Zakabluk <a.zakabluk@admitad.com>
2023-04-28 19:14:09 +0200

    internal/devops-3364-add-user-data

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "yandex_compute_instance" "vps" {
   metadata = {
     ssh-keys = "${var.ssh_username}:${file("${var.ssh_pubkey}")}"
     serial-port-enable = var.serial-port-enable != null ? var.serial-port-enable : null
-    user-data = "${file("${path.module/var.cloud-init-file}")}"
+    user-data = file("${path.module}/${var.cloud-init-file}")
   }
 
   allow_stopping_for_update = true
