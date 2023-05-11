2c8402996357d8698848131bc6bfce59b1587888
Dmitry Trashchynski <d.trashchynski@admitad.com>
2023-05-11 15:14:29 +0200

    add lf

--- main.tf
+++ main.tf
@@ -46,11 +46,6 @@ resource "yandex_compute_instance" "vps" {
     }
   }
 
-  lifecycle {
-    ignore_changes        = [image_id]
-    create_before_destroy = true
-  }
-
   dynamic "secondary_disk" {
     for_each = var.secondary_disk
     content {
